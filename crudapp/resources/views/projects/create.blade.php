4e8ac1f7b16987086365fe1cad86e4e1ba047c0d
yisha819 <haileybaterina@gmail.com>
2026-04-01 21:43:33 +0800

    Fixed button for saving input in tables

--- crudapp/resources/views/projects/create.blade.php
+++ crudapp/resources/views/projects/create.blade.php
@@ -62,12 +62,12 @@
                         </div>
 
                         <div class="flex items-center pt-4">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 focus:bg-indigo-500 active:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            <x-primary-button>
                                 <svg class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                                   <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                                 </svg>
                                 Save Project
-                            </button>
+                            </x-primary-button>
                         </div>
                     </form>
                 </div>
