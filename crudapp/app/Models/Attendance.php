48954d4bae60d1d6dbd9dc59718c81797028ab8a
yisha819 <haileybaterina@gmail.com>
2026-04-08 15:59:10 +0800

    Added attendances table

---
+++ crudapp/app/Models/Attendance.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'date',
+        'check_in',
+        'check_out',
+        'status',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
